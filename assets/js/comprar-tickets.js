12e3cdfeb2ac1483833d77b70a93e93d6f5459cc
Matias Tailler <matiast86@gmail.com>
2023-06-03 18:54:32 -0300

    TrabajoTermindado

--- assets/js/comprar-tickets.js
+++ assets/js/comprar-tickets.js
@@ -1,11 +1,59 @@
-//crear options en array con js
-let arrayCategoria = ['seleccione una opción','Estudiante','Trainee','Junior'];
-let options="";
-arrayCategoria.map((op,i)=> {
-    options+=`<option value="${op}" id="${i}">${op}<option>`
-})
-document.getElementById("categoria").innerHTML=options;
+//identificar la categoría
+identificarAsistente = () => {
+    const persona = document.getElementById('categoria');
+    const asistente = persona.value;
+        return asistente;
+}
+
+//identificar cantidad
+identificarCantidad = () => {
+const apuntadorAsistentes = document.getElementById('cantidad')
+const valorAsistentes = apuntadorAsistentes.value
+const numeroAsistentes = toNumber(valorAsistentes)
+    return numeroAsistentes
+}
+
+function toNumber(valor) {
+    const apuntadorValor = Number(valor);
+    return apuntadorValor;
+}
 
 
+/*function calcularEntrada() {
+    //identificar categoria
+    //const persona = document.getElementById('categoria');
+    //const asistente = persona.value;
 
+    //console.log(asistente)
+
+    //identificar cantidad
+    const apuntadorAsistentes = document.getElementById('cantidad')
+    const valorAsistentes = apuntadorAsistentes.value
+    const numeroAsistentes = toNumber(valorAsistentes)
+
+    //apuntador al precio
+    const apuntarPrecio = document.getElementById('totalAPagar');
+    const asignarPrecio = apuntarPrecio.innerHTML
+}*/
+
+function calcularDescuento() {
+    let total;
+    let unaCantidad = identificarCantidad()
+    let unaCategoria = identificarAsistente()
+
+    switch(unaCantidad, unaCategoria) {
+        case 'estudiante':
+            total = unaCantidad * (200 * 0.2);
+            break;
+        case 'trainee':
+            total = unaCantidad * (200 * 0.5);
+            break;
+        case 'junior':
+            total = unaCantidad * (200 * 0.85);
+            break;
+    }
+    //apuntador al precio
+    const apuntarPrecio = document.getElementById('totalAPagar');
+    apuntarPrecio.innerHTML = total
+}
 
